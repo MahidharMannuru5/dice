7f902f5ee3810f936b49a07d7a3450329b830110
MahidharMannuru5 <81514511+MahidharMannuru5@users.noreply.github.com>
2021-07-01 19:31:28 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-
-const firstRandomNum=Math.floor(Math.random()*6)+1
-const firstImage='assets/dice'+firstRandomNum+".jpg"
-document.querySelectorAll('img')[0].setAttribute('src',firstImage)
-
-
-const secondRandomNum=Math.floor(Math.random()*6)+1
-const secondImage='assets/dice'+secondRandomNum+".jpg"
-document.querySelectorAll('img')[1].setAttribute('src',secondImage)
